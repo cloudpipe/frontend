68fb18d10c3b184077dec4bf7198601dcf299a0e
Ash Wilson <smashwilson@gmail.com>
2015-02-09 12:02:27 -0500

    Store and load sessions to .*storage.

--- app/models/session.js
+++ app/models/session.js
@@ -3,4 +3,16 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   accountName: DS.attr('string'),
   apiKey: DS.attr('string'),
+
+  // Persist this session within localStorage or sessionStorage.
+  storeOn: function(storage) {
+    storage.setItem('accountName', this.get('accountName'));
+    storage.setItem('apiKey', this.get('apiKey'));
+  },
+
+  // Reconstitute this session from localStorage or sessionStorage.
+  loadFrom: function(storage) {
+    this.set('accountName', storage.getItem('accountName'));
+    this.set('apiKey', storage.getItem('apiKey'));
+  }
 });
--- tests/unit/models/session-test.js
+++ tests/unit/models/session-test.js
@@ -2,14 +2,44 @@ import {
   moduleForModel,
   test
 } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('session', 'Session', {
   // Specify the other units that are required for this test.
   needs: []
 });
 
-test('it exists', function() {
+test('it persists itself in local or sessionStorage', function () {
+  var model = this.subject({
+    accountName: 'me@gmail.com',
+    apiKey: '12345'
+  });
+
+  var mock = {
+    setItem: function (key, item) { mock[key] = item; }
+  };
+
+  model.storeOn(mock);
+
+  strictEqual(mock.accountName, 'me@gmail.com');
+  strictEqual(mock.apiKey, '12345');
+});
+
+test('it reconstitutes itself from local or sessionStorage', function () {
   var model = this.subject();
-  // var store = this.store();
-  ok(!!model);
+
+  var mock = {
+    accountName: 'myself@blah.com',
+    apiKey: '54321',
+    getItem: function(key) {
+      return mock[key];
+    }
+  };
+
+  Ember.run(function () {
+    model.loadFrom(mock);
+  });
+
+  strictEqual(model.get('accountName'), 'myself@blah.com');
+  strictEqual(model.get('apiKey'), '54321');
 });
