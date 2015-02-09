efb9e3e3120b1a97737dde0f75a2c18110080859
Ash Wilson <smashwilson@gmail.com>
2015-02-09 13:25:55 -0500

    Hey look an actual test

--- tests/unit/controllers/job-test.js
+++ tests/unit/controllers/job-test.js
@@ -2,14 +2,21 @@ import {
   moduleFor,
   test
 } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('controller:job', 'JobController', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
-// Replace this with your real tests.
-test('it exists', function() {
-  var controller = this.subject();
-  ok(controller);
+var MockJob = Ember.Object.extend({});
+
+test('it capitalizes the job status', function() {
+  var
+    controller = this.subject(),
+    job = MockJob.create({status: 'what'});
+
+  controller.set('model', job);
+
+  strictEqual(controller.get('status'), 'What');
 });
